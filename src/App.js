a3d8bc7fa04ec805942e3382915929997d911c32
unknown <montrae@montraejemison.com>
2022-04-05 20:04:39 -0500

    Connected axios to the backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SongTable from './Components/SongTable/SongTable';
+import axios from "axios";
 
 function App() {
 
-  const[entries,setEntries] = useState([{title: "Drive My Car", album:"Rubber Soul", artist: "The Beatles", genre: "Folk Rock", releaseDate: "12/03/1965"}])
-
-  return (
+const[entries,setEntries] = useState([{title: "Drive My Car", album:"Rubber Soul", artist: "The Beatles", genre: "Folk Rock", releaseDate: "12/03/1965"}])
+//async f(x) with Axios to retrieve the song from the database and then console log the response from the server
+const getAllSongs = async () => {
+  try{
+    let response = await axios.get('http://www.devcodecampmusiclibrary.com/api/music') 
+    setEntries(response.data)
+  }
+  catch{
+    console.log ("file not found") //is that supposed to say "file not found"?
+  } 
+}
+useEffect (()=>{//runs at different points...here it will run at the beginning
+getAllSongs ()
+})
+return (
     <div>
       <SongTable parentEntries = {entries}/>
        
